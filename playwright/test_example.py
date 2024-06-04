e8a8f06844ba79f368a7e410bf860e55a5038355
Ailian482 <1479844033@qq.com>
2024-06-04 12:46:41 +0800

    add

--- playwright/test_example.py
+++ playwright/test_example.py
@@ -1,15 +1,16 @@
 # -*- coding:utf-8 -*-
 # 作    者：Ailian
 # 开发时间：2024/6/3 23:09
+
 import re
 from playwright.sync_api import Page, expect
 
 
 def test_has_title(page: Page):
-    page.goto("https://playwright.dev/")
+    page.goto("https://www.baidu.com/")
 
     # Expect a title "to contain" a substring.
-    expect(page).to_have_title(re.compile("Playwright"))
+    expect(page).to_have_title(re.compile("百度一下，你就知道"))
 
 
 def test_get_started_link(page: Page):
