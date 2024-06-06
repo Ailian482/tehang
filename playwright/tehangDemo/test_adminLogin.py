ca20edcb382fb1b664bc5169d9bb96a41c6a7915
Ailian482 <1479844033@qq.com>
2024-06-07 01:06:52 +0800

    add

--- playwright/tehangDemo/test_adminLogin.py
+++ playwright/tehangDemo/test_adminLogin.py
@@ -1,4 +1,4 @@
-import re
+import time
 from playwright.sync_api import Page, expect
 from playwright.sync_api import sync_playwright, Playwright
 
@@ -10,4 +10,6 @@ def test_admin_login(page: Page):
     page.locator('xpath=//button').click()
     locator = page.locator('xpath=//span[contains(text(),"朱秀君")]')
     expect(locator).to_contain_text('朱秀君')
-
+    # screenshot
+    time.sleep(3)
+    page.screenshot(path='./screenshot/test.png', full_page=True)
